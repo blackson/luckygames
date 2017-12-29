104581e4a734031d484be048dc702032eae7b23a
hotandinh <33078463+hotandinh@users.noreply.github.com>
2017-12-29 22:28:29 +0700

    Create dinhht.js

---
+++ dinhht.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         LuckyDICEBOT
+// @namespace    https://www.facebook.com/lanhhuyet510
+// @version      2.0
+// @description  3333
+// @author       Ngo Anh Duc
+// @match        https://luckygames.io/*
+// @require      https://raw.githubusercontent.com/lanhhuyet510/LuckyGames/master/ETH/DICE_ETH_update3.js
+// @grant        none
+// ==/UserScript==
